1e7a43e32f9568b914c1817663fc097922ce29f7
coutinhocouto <coutinhocouto@hotmail.com>
2020-06-29 00:07:35 -0300

    1.0

--- custom.js
+++ custom.js
@@ -1,7 +1,15 @@
 // JavaScript Document
 $(document).ready(function(){
 
-    
+    $("header").sticky({topSpacing:0});
+	
+	$('.the-page').prepend('<header><div class="container"><div class="row"><div class="col-3"><i onclick="openNav()" class="bx bx-menu-alt-left"></i></div><div class="col-6"><img src="img/logotipo.png" alt="" /></div><div class="col-3"><a href="minha-conta.html"><i class="bx bxs-user-circle"></i></a></div></div></div></header><div class="container"><form action="#" method="get"><input type="text" placeholder="Pesquisar" class="search-top" /></form></div>');
+	
+	$('.the-page').append('<div id="mySidenav" class="sidenav"><a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a><div class="row"><div class="col-12 user-dsk"><img src="img/user-dummy-pic.png" alt="" />	<a href="minha-conta.html">Entrar | Cadastre-se</a>	</div>	<div class="col-12 side-menu"><h3>MENU</h3>	<a href="inde.html"><i class="bx bx-home-alt" ></i> Home</a><a href="pedidos.html"><i class="bx bxs-package"></i> Meus Pedidos</a><a href="carrinho.html"><i class="bx bx-cart-alt" ></i> Carrinho</a>	<a href="favoritos.html"><i class="bx bx-heart" ></i> Favoritos</a><a href="categorias.html"><i class="bx bx-list-ul" ></i> Categorias</a><a href="promocoes.html"><i class="bx bxs-purchase-tag"></i> Promoções</a><a href="configuracoes.html"><i class="bx bx-cog" ></i> Configurações</a></div></div></div>');
+	
+	$('.the-page').append('<footer><div class="container"><div class="row"><div class="col-2"><a href="index.html"><i class="bx bxs-store"></i></a></div><div class="col-2"><a href="descontos.html"><i class="bx bxs-discount"></i></a></div><div class="col-4"><a href="carrinho.html" class="big"><i class="bx bxs-shopping-bag"></i></a></div><div class="col-2"><a href="favoritos.html"><i class="bx bxs-heart"></i></a></div><div class="col-2"><a href="minha-conta.html"><i class="bx bxs-user-circle"></i></a></div></div></div></footer>');
+	
+	$("header").sticky({topSpacing:0});
 
 });
 
@@ -35,13 +43,13 @@ window.addEventListener('load', function () {
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
 	document.getElementById("mySidenav").style.width = "330px";
-	document.getElementById("main").style.marginLeft = "330px";
+	document.getElementById("app").style.marginLeft = "330px";
   }
   
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
 	document.getElementById("mySidenav").style.width = "0";
-	document.getElementById("main").style.marginLeft = "0";
+	document.getElementById("app").style.marginLeft = "0";
 } 
 
 function cutString(s, n){
